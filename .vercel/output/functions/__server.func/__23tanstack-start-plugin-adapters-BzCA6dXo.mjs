bcbf07d74c11d03ff8e1be8d17d971c6913a3918
ahmedzohdy207 <webtryo@proton.me>
2026-06-23 19:13:26 +0300

    Remove .vercel from tracking to fix Vercel deployment

--- .vercel/output/functions/__server.func/__23tanstack-start-plugin-adapters-BzCA6dXo.mjs
+++
@@ -1,5 +0,0 @@
-//#region node_modules/.nitro/vite/services/ssr/assets/__23tanstack-start-plugin-adapters-BzCA6dXo.js
-var pluginSerializationAdapters = [];
-var hasPluginAdapters = false;
-//#endregion
-export { hasPluginAdapters, pluginSerializationAdapters };
